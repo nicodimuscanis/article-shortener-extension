9fa9dbdc638ba80519bff6057372f88591d61788
Dmitry Tatarinov <dmitry.tatarinov@exafore.com>
2023-06-30 13:35:01 +0300

    Add cosmetic changes and description

--- popup.js
+++ popup.js
@@ -3,18 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var currentUrl = tabs[0].url;
     var endpoint = 'https://300.ya.ru/api/sharing-url';
 
-    // Получаем сохраненный токен из хранилища расширения, хотя и без него работает
+    // Получаем сохраненный токен из хранилища
     chrome.storage.sync.get(['token'], function(result) {
       var token = result.token;
-
-      // Отображаем токен на экране
-      //var tokenDisplay = document.getElementById('token-display');
-      //tokenDisplay.textContent = 'Токен: ' + token;
 
       var xhr = new XMLHttpRequest();
       xhr.open('POST', endpoint);
       xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.setRequestHeader('Authorization', 'OAuth ' + token); // Используем сохраненный токен
+      xhr.setRequestHeader('Authorization', 'OAuth ' + token);
       xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
           if (xhr.status === 200) {
